964e306108667e3741624e260886947ba6a25c98
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2023-09-17 18:17:23 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryItemMarkup = createGalleryItemMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryItemMarkup);
+
+function createGalleryItemMarkup(galleryItems){
+    return galleryItems.map(({preview, original, description}) =>{
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    }).join('');
+};
+
+gallery.addEventListener("click", onGalleryClick)
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const parentLink = event.target.closest('.gallery__link');
+    if (!parentLink) {
+        return;
+    }
+    const hrefImageOriginal = parentLink.getAttribute('href');
+
+    const instance = basicLightbox.create(`
+    <img src="${hrefImageOriginal}">
+    `);
+    instance.show();
+}
